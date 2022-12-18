1b8b323684f404d2ffd56c48754271822bc17c89
ammarfarras <xpreuner2@gmail.com>
2022-12-18 15:34:14 +0700

    upload tugas

---
+++ JS/Pertemuan11/data/students.js
@@ -0,0 +1,24 @@
+const students = [
+    {
+        nama: "Ammar Farras Hudzaifah",
+        rombel: "TI09",
+        nim: "0110221222",
+    },
+    {
+        nama: "Zahid Humamun Nafi",
+        rombel: "TI05",
+        nim: "0110221138",
+    },
+    {
+        nama: "Dandi Rasyid",
+        rombel: "TI05",
+        nim: "0110221134",
+    },
+    {
+        nama: "Nuzurwan patri Arja",
+        rombel: "TI09",
+        nim: "0110221230",
+    },
+];
+
+module.exports = students;
